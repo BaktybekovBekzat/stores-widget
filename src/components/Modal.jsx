80e721ea468782d4bbc4132a88e07d7750774bb8
Bekzat Baktybekov <baktybekovbekzat4@gmail.com>
2022-10-12 10:55:59 +0600

    changed swiper

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import "../../node_modules/swiper/swiper-bundle.css";
+import "../../node_modules/swiper/swiper.min.css";
 import { EffectCards } from "swiper";
 import CardItem from "./CardItem";
 
